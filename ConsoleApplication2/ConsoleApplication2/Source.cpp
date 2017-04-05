5cc4a9cd5ca996c73fd92213ab5d18ec9639c26a
ip133225 <zai.cruz@rocketmail.com>
2017-04-05 09:47:36 -0500

    Update

--- ConsoleApplication2/ConsoleApplication2/Source.cpp
+++ ConsoleApplication2/ConsoleApplication2/Source.cpp
@@ -8,6 +8,24 @@
 int main()
 {
 
-	double 
+	int remainingEggs, dozenEggs, someEggs;
+	double dozenPrice, remainingEggCost, grandTotal;
+	std::cout << "How many eggs would you like? " << std::endl;
+	std::cin >> someEggs;
+	std::cout << "" << std::endl;
+
+	remainingEggs = someEggs % 12;
+
+	remainingEggCost = remainingEggs * 0.25;
+
+	dozenEggs = someEggs / 12;
+
+	dozenPrice = dozenEggs * 2;
+
+	grandTotal = dozenPrice + remainingEggCost;
+
+	std::cout << "You have " << dozenEggs << " dozen eggs for a total price of $" << dozenPrice << ".00\nwith remiander of " << remainingEggs << " eggs for a total price of $" << remainingEggCost << ". " << std::endl;
+	std::cout << "" << std::endl;
+	std::cout << "For a grand total of $" << grandTotal << "! Thank you for shopping with me have a wonderful day." << std::endl;
 
 }
