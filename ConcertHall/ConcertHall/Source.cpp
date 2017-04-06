3c7fb42de6763b898eca36dfd05e2c75731d478d
ip133225 <zai.cruz@rocketmail.com>
2017-04-06 14:14:05 -0500

    Updaye

--- ConcertHall/ConcertHall/Source.cpp
+++ ConcertHall/ConcertHall/Source.cpp
@@ -1,2 +1,40 @@
 // Concert Hall 
-// 
+// Isaiah Ponciano
+//Date: 3/30/17
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+
+	int orchestra_Seats, mainFloor_Seats, balcony_Seats, orchestra_Seats_Cost, mainFloor_Seats_Cost, balcony_Seats_Cost, allSeats;
+
+
+
+	std::cout << "Please enter how many tickets sold for Orchestra: ";
+	std::cin >> orchestra_Seats;
+
+	std::cout << "Please enter how many tickets sold for Main Floor: ";
+	std::cin >> mainFloor_Seats;
+
+	std::cout << "Please enter how many tickets sold for Balcony: ";
+	std::cin >> balcony_Seats;
+
+	orchestra_Seats_Cost = orchestra_Seats * 25;
+	mainFloor_Seats_Cost = mainFloor_Seats * 30;
+	balcony_Seats_Cost = balcony_Seats * 15;
+	allSeats = orchestra_Seats_Cost + mainFloor_Seats_Cost + balcony_Seats_Cost;
+
+	std::cout << " " << std::endl;
+	std::cout << "For the Orchestra seats the cost is $" << orchestra_Seats_Cost << "." << std::endl;
+	std::cout << "For the Main Floor seats the cost is $" << mainFloor_Seats_Cost << "." << std::endl;
+	std::cout << "For the Balcony seats the cost is $" << balcony_Seats_Cost << "." << std::endl;
+	std::cout << " " << std::endl;
+	std::cout << "The total over all is $" << allSeats << "." << std::endl;
+
+
+
+	system("pause");
+	return 0;
+}
