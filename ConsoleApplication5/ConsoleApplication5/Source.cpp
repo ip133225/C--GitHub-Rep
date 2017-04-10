f5773ba15362e80ccd3ac0dee58f00281b9e3a1a
ip133225 <zai.cruz@rocketmail.com>
2017-04-10 10:57:24 -0500

    Update

---
+++ ConsoleApplication5/ConsoleApplication5/Source.cpp
@@ -0,0 +1,31 @@
+//ageProgram
+//Isaiah Ponciano
+//Date 4/07/17
+
+#include <iostream>
+#include string
+
+main()
+{
+	int age;
+
+	std::cout << "Enter your age.";
+	std::cin >> age;
+
+	if (age <= 11)
+		std::cout << "You are 10 or less, " << std::endl;
+	if (age<5)
+		"" 
+
+
+	else if (age<=12 && age < 18)
+		std::cout << "You are les than 18 nut older than 12" << std::endl;
+
+	else if (age >= 19 && age <= 30)
+		std::cout << "Still in your 20's enjoy it" << std::endl;	
+	
+	
+	
+	system("pause");
+	return 0;
+}
