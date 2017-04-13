dcfc0139cf792b0c62c9cb50fb46d21551d94922
ip133225 <zai.cruz@rocketmail.com>
2017-04-13 11:09:02 -0500

    Update

--- ConsoleApplication3/ConsoleApplication3/Source.cpp
+++ ConsoleApplication3/ConsoleApplication3/Source.cpp
@@ -8,29 +8,42 @@
 int main()
 {
 
-	int perNightRate, amountOfNights, serviceCharge, perMinute, serviceTimes, serviceChargeYes_NO, grandTotal, totalAmount;
-	double phoneCharge;
-
-	std::cout << "How many nights did they stay? " << std::endl;
+	double perNightRate, amountOfNights, perMinute, serviceChargeYes_NO,  totalAmount;
+	double phoneCharge, grandTotal;
+	std::string anotherNightYes_NO;
+	double serviceCharge = 35;
+	int extraNight=0;
+	std::cout << "How many nights would you like to stay? " << std::endl;
 	std::cin >> amountOfNights;
+	std::cout << " " << std::endl;
 
-	std::cout << "Did they pay for the room service? " << std::endl;
-	std::cin >> serviceChargeYes_NO; 
-
-	std::cout << "How many minutes where they on the phone? " << std::endl;
+	std::cout << "How many minutes where you on the phone? " << std::endl;
 	std::cin >> perMinute;
+	
+		do{
 
-	serviceCharge = serviceChargeYes_NO * 35;
+
+			std::cout << " " << std::endl;
+
+			std::cout << "Would you like to stay another night? " << std::endl;
+			std::cin >> anotherNightYes_NO;
+			extraNight = extraNight + 100;
+			std::cout << " " << std::endl;
+
+		} while (anotherNightYes_NO == "Yes");
+	
 	phoneCharge = perMinute * .25;
-	perNightRate = amountOfNights * 100;
+	perNightRate = amountOfNights * 100 + extraNight;
 	totalAmount = perNightRate + serviceCharge;
 	grandTotal = totalAmount + phoneCharge;
 
 	std::cout << "They have to pay $ " << perNightRate << " for rent " << std::endl;
+	std::cout << "" << std::endl;
 	std::cout << "They have to pay $ " << serviceCharge << " for the room service. " << std::endl;
+	std::cout << "" << std::endl;
 	std::cout << "They have to pay $ " << phoneCharge << "0 for the phone bill. " << std::endl;
 	std::cout << "" << std::endl;
-	std::cout << "Their total bill comes to $" << grandTotal << "." << std::endl;
+	std::cout << "Your total bill comes to $" << grandTotal << "." << std::endl;
 
 	system("pause");
 	return 0;
---
+++ ConsoleApplication7/ConsoleApplication7/Source.cpp
@@ -0,0 +1,13 @@
+//Right Triangle
+//Isaiah Ponciano
+//Date: 4/12/17
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+	std::string whichSide, a, A, b, B, c, C;
+
+	int responds = 0;
+}
